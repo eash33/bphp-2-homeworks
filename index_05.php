8667543363691736f4d3e47dd8542be75f53647f
Evgenia Shulakova <eash33@gmail.com>
2026-02-09 11:45:19 +0300

    Update index_05.php

--- index_05.php
+++ index_05.php
@@ -3,31 +3,26 @@ mb_internal_encoding('UTF-8');
 mb_regex_encoding('UTF-8');
 
 echo "Введите Ваше имя: ";
-$name = trim(readline());
+$name = trim(readline()); 
 
 echo "Введите Вашу фамилию: ";
 $surname = trim(readline());
 
 echo "Введите Ваше отчество: ";
-$patronymic = trim(readline());
-
+$patronymic = trim(readline()); 
 
 $fullName = $surname . " " . $name . " " . $patronymic;
 $fullName = mb_convert_case($fullName, MB_CASE_TITLE, "UTF-8");
 echo "Полное имя: $fullName".PHP_EOL;
 
+$initial = mb_strtoupper(mb_substr($name,0,1,"UTF-8"),"UTF-8").".".
+mb_strtoupper(mb_substr($patronymic,0,1,"UTF-8"),"UTF-8").".";
 
-if (preg_match('/^(\p{L}+)\s+(\p{L}+)\s+(\p{L}+)$/u', $fullName, $matches)) {
-    $surnameAndInitials = mb_ucfirst($matches[1], 'UTF-8');
-    $initials = mb_strtoupper(mb_substr($matches[2], 0, 1, 'UTF-8'), 'UTF-8') . '.' .
-                mb_strtoupper(mb_substr($matches[3], 0, 1, 'UTF-8'), 'UTF-8') . '.';
-    $fio = mb_strtoupper(mb_substr($matches[1], 0, 1, 'UTF-8'), 'UTF-8') .
-            mb_strtoupper(mb_substr($matches[2], 0, 1, 'UTF-8'), 'UTF-8') .
-            mb_strtoupper(mb_substr($matches[3], 0, 1, 'UTF-8'), 'UTF-8');
+$surname = mb_convert_case($surname, MB_CASE_TITLE, "UTF-8");
+$surnameAndInitials = $surname." ". $initial;
+echo "Фамилия и инициалы: $surnameAndInitials".PHP_EOL;
 
-    echo "Фамилия и инициалы: {$surnameAndInitials} {$initials}" . PHP_EOL;
-    echo "Аббревиатура: {$fio}" . PHP_EOL;
-} else {
-    echo "Не удалось распознать ФИО. Убедитесь, что корректно заполнили поля (Фамилия Имя Отчество)." . PHP_EOL;
-
-};
+$fio = mb_strtoupper(mb_substr($surname,0,1,"UTF-8"),"UTF-8"). 
+mb_strtoupper(mb_substr($name,0,1,"UTF-8"),"UTF-8").
+mb_strtoupper(mb_substr($patronymic,0,1,"UTF-8"),"UTF-8");
+echo "Аббревиатура: $fio".PHP_EOL;
